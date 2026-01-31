fb41a9e7f9876035f711dce270239e561b81d3b7
荣顶 <wangrongding@qq.com>
2026-01-31 21:52:34 +0800

    chore: 更新 clash-kit 版本至 1.1.2，并更新对应的 sha256 校验值

--- Formula/clash-kit.rb
+++ Formula/clash-kit.rb
@@ -3,8 +3,8 @@ require "language/node"
 class ClashKit < Formula
   desc "A command-line interface for managing Clash configurations, subscriptions, and proxies"
   homepage "https://github.com/wangrongding/clash-kit"
-  url "https://registry.npmjs.org/clash-kit/-/clash-kit-1.1.0.tgz"
-  sha256 "39f8f5d1fcc988348399e593223b1c1552b2be4ee1a7234c49ea10bd9ef42671"
+  url "https://registry.npmjs.org/clash-kit/-/clash-kit-1.1.2.tgz"
+  sha256 "97c689a8bcb6c1da41d9f4b15fb5564f3a904f68f6c2da69436c11123c1a7858"
   license "ISC"
 
   depends_on "node"
